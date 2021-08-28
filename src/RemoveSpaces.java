7bcdf28cb1a39a30ba5cb35e562943d3f1931124
Evgeny Tkach <1eluzive1@gmail.com>
2021-08-28 18:12:30 +0300

    Create RemoveSpaces.java

---
+++ src/RemoveSpaces.java
@@ -0,0 +1,11 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        System.out.println(
+        noSpace("8 j 8   mBliB8g  imjB8B8  jl  B")
+        );
+    }
+
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+}
