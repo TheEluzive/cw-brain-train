9a74a56d4b0e857c1e4a2e2d1076b0a7e5318361
Evgeny Tkach <1eluzive1@gmail.com>
2021-08-29 11:55:13 +0300

    Update Persist.java

--- src/Persist.java
+++ src/Persist.java
@@ -2,25 +2,25 @@ import java.util.Arrays;
 
 public class Persist {
     public static void main(String[] args) {
-        System.out.println(persistence(99));
+        System.out.println(persistence(25));
     }
     public static int persistence(long n) {
         int  r = 0;
         long[] digits;
+        long j = 1;
 
         while (n >= 10) {
 
             digits = new long[Long.toString(n).length()];
-           
+
+            j = 1;
             for (int i = 0; i < digits.length; i++) {
                 digits[i] = n % 10;
                 n /= 10;
+                j = j * digits[i];
             }
+            n = j;
 
-            n = 1;
-            for (long v : digits) {
-                n = n * v;
-            }
             r++;
         }
         return r;
