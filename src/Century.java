c99587192079d267d0aba5171c6ccf308685614a
Evgeny Tkach <1eluzive1@gmail.com>
2021-08-29 10:17:13 +0300

    Update Century.java

--- src/Century.java
+++ src/Century.java
@@ -17,7 +17,7 @@ public class Century {
     public static int century(int number) {
         /*return (double)number  % 100 == 0 ?
                 number / 100  : number / 100 + 1;*/
-        return (number - 1) / 100 + 1;
+        return (number + 99) / 100;
 
     }
 }
