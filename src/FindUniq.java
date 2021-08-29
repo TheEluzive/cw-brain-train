7f2612d890b631a965b7dc3ace4edfb255b5971c
Evgeny Tkach <1eluzive1@gmail.com>
2021-08-29 09:50:03 +0300

    Create FindUniq.java

---
+++ src/FindUniq.java
@@ -0,0 +1,22 @@
+public class FindUniq {
+    public static void main(String[] args) {
+
+    }
+    public static double findUniq(double[] arr) {
+        double defaultNumber = 0;
+        if (arr[0] == arr [1] && arr[1] == arr [2]) {
+            defaultNumber = arr[0];
+            for (double v : arr) {
+                if (v != defaultNumber) return v;
+            }
+        }
+        else
+            if ( arr[0] == arr[1]) return arr[2];
+                else
+                    if (arr[0] == arr[2]) return arr[1];
+                    else return arr[0];
+
+        // Do the magic
+        return arr[0];
+    }
+}
