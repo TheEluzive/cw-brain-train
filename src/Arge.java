75e7ef2e634fcc8df51a8e13ba207a60f96068da
Evgeny Tkach <1eluzive1@gmail.com>
2021-08-30 23:34:34 +0300

    Create Arge.java

---
+++ src/Arge.java
@@ -0,0 +1,17 @@
+public class Arge {
+    public static void main(String[] args) {
+        System.out.println(
+                Arge.nbYear(1500, 5, 100, 5000)
+        );
+    }
+
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int year = 0;
+        percent = percent / 100  + 1 ;
+        while (p0 < p){
+            p0 = (int) (p0 * percent) + aug;
+            year++;
+        }
+        return year;
+    }
+}
