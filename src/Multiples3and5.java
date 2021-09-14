425692670397ab0d83e631e1797a7100e1f1f898
Evgeny Tkach <1eluzive1@gmail.com>
2021-09-14 13:29:39 +0300

    Update Multiples3and5.java

--- src/Multiples3and5.java
+++ src/Multiples3and5.java
@@ -1,20 +1,23 @@
 import org.junit.jupiter.api.Test;
 
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Multiples3and5 {
 
     public int solution(int number) {
-        int sum = 0;
-        for (int i = 0; i < number; i++) {
-            if ((i % 3 == 0 || i % 5 == 0)) {
-                sum = sum + i;
-            }
-        }
-
-
-        return sum;
+//        int sum = 0;
+//        for (int i = 0; i < number; i++) {
+//            if ((i % 3 == 0 || i % 5 == 0)) {
+//                sum = sum + i;
+//            }
+//        }
+//
+//        return sum;
+        return IntStream.range(0, number).filter(o -> o % 3 == 0 || o % 5 == 0)
+                .sum();
         //TODO: Code stuff here
     }
 
